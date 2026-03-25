dfa80d5f2257244675efdda6680287c224e6c12f
nwemaythet30-cmyk <nwemaythet30@gmail.com>
2026-03-25 23:49:29 +0800

    Add project files.

---
+++ MyDigiBooth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyDigiBooth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
